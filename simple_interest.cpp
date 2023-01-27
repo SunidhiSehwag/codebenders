d81cff0e0b2cb7437b97ec09b5441084b00b55e8
SunidhiSehwag <120239019+SunidhiSehwag@users.noreply.github.com>
2023-01-27 16:49:24 +0530

    submitting question 2

---
+++ simple_interest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r;
+    int si,p,t;
+    cin>>p>>r>>t;
+    si=(p*r*t)/100;
+    cout<<si;
+
+    return 0;
+}
